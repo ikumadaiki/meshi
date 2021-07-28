1a4edd3d42359ec238b40e624f2c16de6d1f7b16
ikumadaiki <ikuma.daiki.infratop@gmail.com>
2021-07-28 01:30:49 +0000

    [Start] 4-4

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '2e264fbe048679b6ae42fc569f18223b0a89456542129fc8f52a3f0f9139d1e87d1a9234aad185b6abc7e43a0764b2433de0feded3f06765079d21138d64b102'
